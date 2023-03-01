0c4a886bc7450ec29909940c3430da912f30c219
CJ <cjfritz9@gmail.com>
2023-03-01 15:59:52 -0700

    removing outdated api key oops

--- src/openai/api.ts
+++ src/openai/api.ts
@@ -1,6 +1,6 @@
 import { Configuration, OpenAIApi } from 'openai';
 const configuration = new Configuration({
   organization: 'org-wg4VQOo1bRRB7bEwcWCetAw1',
-  apiKey: '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'
+  apiKey: ''
 });
 export const openai = new OpenAIApi(configuration);
